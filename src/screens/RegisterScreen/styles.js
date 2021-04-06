4f598b1e6af69aaca767995f19f58e95469aad76
Marvin Ebuenga <marvinebuenga.cs@gmail.com>
2021-04-07 07:35:31 +0800

    Added Register Screen

---
+++ src/screens/RegisterScreen/styles.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: '#FFFFFF',
+  },
+  inputContainer: {
+    width: 300,
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+});
+export default styles;
